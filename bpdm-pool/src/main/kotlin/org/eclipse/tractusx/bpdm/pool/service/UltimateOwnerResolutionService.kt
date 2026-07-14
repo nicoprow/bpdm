971b1dea91d80f889e81c8445aac5ad7c8056e4a
Nico Koprowski <nico.koprowski@abat.de>
2026-07-14 15:43:04 +0800

    Make resolveUltimateOwner transactional to keep a session for lazy validityPeriods

--- bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/service/UltimateOwnerResolutionService.kt
+++ bpdm-pool/src/main/kotlin/org/eclipse/tractusx/bpdm/pool/service/UltimateOwnerResolutionService.kt
@@ -41,6 +41,7 @@ class UltimateOwnerResolutionService(
     private val logger = KotlinLogging.logger { }
 
 
+    @Transactional(readOnly = true)
     fun resolveUltimateOwner(legalEntity: LegalEntityDb): String? {
         val visited = mutableSetOf<String>()
         return resolveUltimateOwnerWithCycleProtection(legalEntity, visited)
